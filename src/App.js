5977b91fb9cb4a0617b346fe3e1a3a3a9887f32b
Hamza Masood <masood.hamzaa@gmail.com>
2021-06-30 21:02:09 +0500

    fetchdata

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [posts, setPosts] = useState(null);
+
+  useEffect(async () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setPosts(json));
+  }, []);
+
   return (
     <div className="App">
       <h1>Fetch data from API</h1>
+      {posts && (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <strong>{post.title}</strong> <br />
+              {post.body}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
